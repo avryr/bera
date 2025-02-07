f2519f2320b3ec75e915a2e0634bb1d63a5608eb
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-02-07 15:00:07 -0500

    placeholder for CWRU data

--- nationalWeatherData.py
+++ nationalWeatherData.py
@@ -7,18 +7,23 @@ import requests #library that makes the calls to the API URL and the response ba
 
 
 #CLE Hopkins zone
-URLKCLE = "https://api.weather.gov/stations/KCLE/observations/latest"
+URL_KCLE = "https://api.weather.gov/stations/KCLE/observations/latest"
 #Burke
-URLKBKL = "https://api.weather.gov/stations/KCLE/observations/latest"
+URL_KBKL = "https://api.weather.gov/stations/KBKL/observations/latest"
+#CWRU
+URL_CWRU = "https://api.weather.gov/stations/KBKL/observations/latest"
 
 #get the response from the server as a JSON file
 #open URL in browser to see full JSON file
-responseKCLE = requests.get(URLKCLE) 
-responseKBKL = requests.get(URLKCLE) 
+responseKCLE = requests.get(URL_KCLE) 
+responseKBKL = requests.get(URL_KBKL) 
+responseCWRU = requests.get(URL_CWRU) 
 
 #get relevant data
 forecastKCLE = responseKCLE.json()['properties']
 forecastKBKL = responseKBKL.json()['properties']
+forecastCWRU = responseCWRU.json()['properties']
+
 
 weatherData = {
     "KCLE": {
@@ -34,6 +39,13 @@ weatherData = {
         "dewpoint": forecastKBKL['dewpoint']['value'],
         "barometricPressure": forecastKBKL['barometricPressure']['value'],
         "relativeHumidity": forecastKBKL['relativeHumidity']['value']
+    },
+    "CWRU": {
+        "timestamp": forecastCWRU['timestamp'],
+        "temperature": forecastCWRU['temperature']['value'],
+        "dewpoint": forecastCWRU['dewpoint']['value'],
+        "barometricPressure": forecastCWRU['barometricPressure']['value'],
+        "relativeHumidity": forecastCWRU['relativeHumidity']['value']
     }
 }
 print(weatherData)
