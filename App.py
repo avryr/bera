8586e820d1e7f5ef7bf8bad275f6f4e2e8055f8a
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-02-28 14:55:15 -0500

    remove loop from App

--- App.py
+++ App.py
@@ -1,26 +1,14 @@
 import processData
 import time
+import schedule
 
 
 def Start():
     locationList = ["CWRU", "NASA"]
     parameterList = ["temperature", "barometricPressure", "relativeHumidity", "dewpoint"]
 
-    flag = False
-    duration = 0
-    while flag == False:
-        try:
-            duration = int(input("How many seconds do you want to run this program? "))
-            flag = True
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
+    processData.CollectData()
+    processData.Graph(locationList, parameterList)
     
-    start_time = time.time()
-    while time.time() - start_time < duration:
-        processData.CollectData()
-        processData.Graph(locationList, parameterList)
-        time.sleep(1)
-        
 
-
-Start()
+schedule.every(30).minutes.do(Start)
