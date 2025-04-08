b343f5417ef5cd0bbfc28f19c486613cc8908576
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-04-08 16:08:55 -0400

    standardized timestamp

--- DataCollection/dataCollection.py
+++ DataCollection/dataCollection.py
@@ -2,6 +2,7 @@ import schedule
 import TempestWeatherData
 import NationalWeatherData
 import database_connection
+from datetime import datetime, timezone
 
 #If you want to make your own project, all you need to do is change the values in this script
 #If you want to use a different MongoDB database, make a different getConnectionString.py script.
@@ -17,12 +18,15 @@ def Start():
     KBKL = NationalWeatherData.NationalWeatherData("KBKL")
 
 
-    database_connection.uploadToDatabase("CWRU", CWRUData)
-    database_connection.uploadToDatabase("KCLE", KCLE)
+    # insert timestamp
+    timestamp = datetime.now(timezone.utc)
+
+    database_connection.uploadToDatabase("CWRU", CWRUData, timestamp)
+    database_connection.uploadToDatabase("KCLE", KCLE, timestamp)
+    database_connection.uploadToDatabase("KCLE", KBKL, timestamp)
     # ADD DATA FROM FLDIGI NOW!!!
     # database_connection.uploadToDatabase("Radio", RadioData)
 
-    # insert timestamp
-    #NOTE: ADD TIMESTAMP TO database_connection function!!!!
+    
 
 schedule.every(30).minutes.do(Start)
