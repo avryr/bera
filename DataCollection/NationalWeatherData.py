176ff99a7a4bb3f8e4196d5d114fdc8980a3c81b
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-04-10 18:37:53 -0400

    NWS error catching

--- DataCollection/NationalWeatherData.py
+++ DataCollection/NationalWeatherData.py
@@ -7,25 +7,30 @@ import requests #library that makes the calls to the API URL and the response ba
 
 def NationalWeatherData(airportCode):
 
-    #CLE Hopkins zone
-    URL = "https://api.weather.gov/stations/" + airportCode + "/observations/latest"
-    #Burke
-
-    #get the response from the server as a JSON file
-    #open URL in browser to see full JSON file
-    response = requests.get(URL) 
-
-    #get relevant data
-    forecast = response.json()['properties']
-
-    weatherData = {
-        "timestamp": datetime.fromisoformat(forecast['timestamp']),
-        "temperature": forecast['temperature']['value'],
-        "dewpoint": forecast['dewpoint']['value'],
-        "barometricPressure": forecast['barometricPressure']['value'],
-        "relativeHumidity": forecast['relativeHumidity']['value'],
-        "precipitation": forecast['precipitationLast6Hours']['value']
-    }
+    try:
+        URL = "https://api.weather.gov/stations/" + airportCode + "/observations/latest"
+        #get the response from the server as a JSON file
+        #open URL in browser to see full JSON file
+        response = requests.get(URL) 
+        #get relevant data
+        forecast = response.json()['properties']
+        weatherData = {
+            "timestamp": datetime.fromisoformat(forecast['timestamp']),
+            "temperature": forecast['temperature']['value'],
+            "dewpoint": forecast['dewpoint']['value'],
+            "barometricPressure": forecast['barometricPressure']['value'],
+            "relativeHumidity": forecast['relativeHumidity']['value'],
+            "precipitation": forecast['precipitationLast6Hours']['value']
+        }
+    except: 
+        weatherData = {
+            "timestamp": datetime.fromisoformat(forecast['timestamp']),
+            "temperature": None,
+            "dewpoint": None,
+            "barometricPressure": None,
+            "relativeHumidity": None,
+            "precipitation": None
+        }
     return weatherData
 
 #print(getWeatherData())
