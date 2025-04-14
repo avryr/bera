4b28c61cd6f93105f2a4743741d375821e3c6407
Andrej <andrej@case.edu>
2025-04-14 12:24:52 -0400

    Formatting improvements

--- app/page.tsx
+++ app/page.tsx
@@ -13,30 +13,30 @@ const defaultLoc1 = 'W8EDU';
 const defaultLoc2 = 'N8OBJ';
 
 //funcs
-function changeLocs(loc1 : string, loc2 : string) {
+function changeLocs(loc1: string, loc2: string) {
     // Set the text of the location spans
     $('#loc1').text(loc1);
     $('#loc2').text(loc2);
 }
-function toggleChart(divID : string) {
+function toggleChart(divID: string) {
     $(divID).toggle();
     console.log(divID + " clicked!");
 }
 
 // React useEffect hook instead of jQuery document ready
 function useInitialization() {
-  useEffect(() => {
-    changeLocs(defaultLoc1, defaultLoc2);
-    // Set checkboxes to unchecked
-    $('input[type=checkbox]').prop('checked', false);
-  }, []); // Empty dependency array means this runs once on mount... I hope
+    useEffect(() => {
+        changeLocs(defaultLoc1, defaultLoc2);
+        // Set checkboxes to unchecked
+        $('input[type=checkbox]').prop('checked', false);
+    }, []); // Empty dependency array means this runs once on mount... I hope
 }
 
- // Register Chart.js components
- Chart.register(...registerables);
+// Register Chart.js components
+Chart.register(...registerables);
 
 // Temperature chart component
-function TemperatureChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
+function TemperatureChart({ dateFrom, dateTo }: { dateFrom: string, dateTo: string }) {
     const [chartData, setChartData] = useState({
         labels: [],
         datasets: [
@@ -62,19 +62,19 @@ function TemperatureChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}
             }
         ]
     });
-    
+
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        
+
         // Fetch bit error rate data
         const berPromise = fetch(`/api/get-chart?metric=bitErrorRate&dateFrom=${dateFrom}&dateTo=${dateTo}`)
             .then(response => {
                 if (!response.ok) throw new Error('Failed to fetch BER data');
                 return response.json();
             });
-        
+
         // Fetch temperature data
         const tempPromise = fetch(`/api/get-chart?metric=temperature&dateFrom=${dateFrom}&dateTo=${dateTo}`)
             .then(response => {
@@ -151,7 +151,7 @@ function TemperatureChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}
 };
 
 //Humidity chart component
-function HumidityChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
+function HumidityChart({ dateFrom, dateTo }: { dateFrom: string, dateTo: string }) {
     const [chartData, setChartData] = useState({
         labels: [],
         datasets: [
@@ -177,7 +177,7 @@ function HumidityChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
             }
         ]
     });
-    
+
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -188,7 +188,7 @@ function HumidityChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
                 if (!response.ok) throw new Error('Failed to fetch BER data');
                 return response.json();
             });
-        
+
         // Fetch humidity data
         const humidityPromise = fetch(`/api/get-chart?metric=relativeHumidity&dateFrom=${dateFrom}&dateTo=${dateTo}`)
             .then(response => {
@@ -199,7 +199,7 @@ function HumidityChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
         // Wait for both requests to complete
         Promise.all([berPromise, humidityPromise])
             .then(([berData, humidityData]) => {
-                
+
                 setChartData({
                     labels: berData.x, // both datasets have the same x values
                     datasets: [
@@ -267,190 +267,190 @@ function HumidityChart({dateFrom, dateTo}: {dateFrom: string, dateTo: string}) {
 
 
 export default function Home() {
-  useInitialization(); // document.ready replacement
-  const defaultDateFrom = "2025-04-07";
-  const defaultDateTo = "2025-04-10";
+    useInitialization(); // document.ready replacement
+    const defaultDateFrom = "2025-04-07";
+    const defaultDateTo = "2025-04-10";
 
-  const [dateFrom, setDateFrom] = useState(defaultDateFrom);
-  const [dateTo, setDateTo] = useState(defaultDateTo);
-  const [dateRender, setDateRender] = useState(true);
+    const [dateFrom, setDateFrom] = useState(defaultDateFrom);
+    const [dateTo, setDateTo] = useState(defaultDateTo);
+    const [dateRender, setDateRender] = useState(true);
 
-  function reloadCharts(){
-    setDateRender(false)
-    setTimeout(() => setDateRender(true), 1);
-  }
-
-
-  function handleDateChange(event : React.ChangeEvent<HTMLInputElement>) {
-    if(event.target.id == 'from'){
-        setDateFrom(event.target.value);
-    }else{
-        setDateTo(event.target.value);
+    function reloadCharts() {
+        setDateRender(false)
+        setTimeout(() => setDateRender(true), 1);
     }
-    reloadCharts();
-  }
-  
 
-  return (
-    <div className="container-fluid">
-        {/* Header */}
-        <div className="row">
-            {/* Logo */}
-            <div className="col-sm-2 pageTitle d-flex justify-content-center align-items-center">
-                <img src="/bera.svg" className="logo" alt="BERA Logo" />
-            </div>
-            {/* Location Display */}
-            <div className="col-sm-10">
-                <div className="row header">
-                    <div className="col-sm-9 d-flex justify-content-center align-items-center" style={{ minHeight: "100%" }}>
-                        <div className="panel panel-body locationDisplay">
-                            <span id="loc1"></span> <i className="arrow fa-solid fa-arrow-right"></i> <span id="loc2"></span>
+
+    function handleDateChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.id == 'from') {
+            setDateFrom(event.target.value);
+        } else {
+            setDateTo(event.target.value);
+        }
+        reloadCharts();
+    }
+
+
+    return (
+        <div className="container-fluid">
+            {/* Header */}
+            <div className="row">
+                {/* Logo */}
+                <div className="col-sm-2 pageTitle d-flex justify-content-center align-items-center">
+                    <img src="/bera.svg" className="logo" alt="BERA Logo" />
+                </div>
+                {/* Location Display */}
+                <div className="col-sm-10">
+                    <div className="row header">
+                        <div className="col-sm-9 d-flex justify-content-center align-items-center" style={{ minHeight: "100%" }}>
+                            <div className="panel panel-body locationDisplay">
+                                <span id="loc1"></span> <i className="arrow fa-solid fa-arrow-right"></i> <span id="loc2"></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-sm-1"></div>
-                    {/* Change Location Button */}
-                    <div className="col-sm-2">
-                        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100%" }}>
-                            <button 
-                                type="button" 
-                                className="btn locationBtn"
-                                onClick={() => {
-                                    console.log("Change Location button clicked!");
-                                }}
-                            >
-                                Change Location
-                            </button>
+                        <div className="col-sm-1"></div>
+                        {/* Change Location Button */}
+                        <div className="col-sm-2">
+                            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100%" }}>
+                                <button
+                                    type="button"
+                                    className="btn locationBtn"
+                                    onClick={() => {
+                                        console.log("Change Location button clicked!");
+                                    }}
+                                >
+                                    Change Location
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        {/* Document */}
-        <div className="row">
-            {/* Sidebar */}
-            <div className="col-sm-2 sidebar">
-                <h1>Data</h1>
-                <hr />
-                {/* Checklist */}
-                <div className="checks">
+            {/* Document */}
+            <div className="row">
+                {/* Sidebar */}
+                <div className="col-sm-2 sidebar">
+                    <h1>Data</h1>
+                    <hr />
+                    {/* Checklist */}
+                    <div className="checks">
+                        {/* Precipitation */}
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="precip"
+                                onChange={() => toggleChart('#precipChart')}
+                            />
+                            <label className="form-check-label" htmlFor="precip">
+                                Precipitation
+                            </label>
+                        </div>
+                        <p></p>
+                        {/* Temperature */}
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="temp"
+                                onChange={() => toggleChart('#tempChart')}
+                            />
+                            <label className="form-check-label" htmlFor="temp">
+                                Temperature
+                            </label>
+                        </div>
+                        <p></p>
+                        {/* Humidity */}
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="humid"
+                                onChange={() => toggleChart('#humidChart')}
+                            />
+                            <label className="form-check-label" htmlFor="humid">
+                                Humidity
+                            </label>
+                        </div>
+                        <p></p>
+                        {/* Dewpoint */}
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="dew"
+                                onChange={() => toggleChart('#dewChart')}
+                            />
+                            <label className="form-check-label" htmlFor="dew">
+                                Dewpoint
+                            </label>
+                        </div>
+                        <p></p>
+                        {/* Pressure */}
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="pressure"
+                                onChange={() => toggleChart('#pressureChart')}
+                            />
+                            <label className="form-check-label" htmlFor="pressure">
+                                Pressure
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                {/* Sidebar Footer */}
+                <div className="col-sm-2 setToBottom">
+                    <div className="dates">
+                        <h4>From</h4>
+                        <input type="date" id="from" className="dateInput" value={dateFrom} onChange={handleDateChange}></input> to <input type="date" id="to" className="dateInput" value={dateTo} onChange={handleDateChange}></input>
+                    </div>
+                    <hr />
+                    <div className="sideLink">
+                        <a href="https://github.com/avryr/bera">Link to our GitHub...</a>
+                    </div>
+                </div>
+                {/* Charts */}
+                <div className="col-sm-10 charts">
                     {/* Precipitation */}
-                    <div className="form-check">
-                        <input 
-                          className="form-check-input" 
-                          type="checkbox" 
-                          id="precip" 
-                          onChange={() => toggleChart('#precipChart')}
-                        />
-                        <label className="form-check-label" htmlFor="precip">
-                            Precipitation
-                        </label>
+                    <div id="precipChart">
+                        <h2>Precipitation</h2>
+                        <hr />
                     </div>
-                    <p></p>
                     {/* Temperature */}
-                    <div className="form-check">
-                        <input 
-                          className="form-check-input" 
-                          type="checkbox" 
-                          id="temp"
-                          onChange={() => toggleChart('#tempChart')}
-                        />
-                        <label className="form-check-label" htmlFor="temp">
-                            Temperature
-                        </label>
+                    <div id="tempChart">
+                        <h2>Temperature</h2>
+                        <hr />
+                        <div className="chart-container" style={{ height: '400px' }}>
+                            {dateRender && <TemperatureChart dateFrom={dateFrom} dateTo={dateTo} />}
+                        </div>
                     </div>
-                    <p></p>
                     {/* Humidity */}
-                    <div className="form-check">
-                        <input 
-                          className="form-check-input" 
-                          type="checkbox" 
-                          id="humid"
-                          onChange={() => toggleChart('#humidChart')}
-                        />
-                        <label className="form-check-label" htmlFor="humid">
-                            Humidity
-                        </label>
+                    <div id="humidChart">
+                        <h2>Humidity</h2>
+                        <hr />
+                        <div className="chart-container" style={{ height: '400px' }}>
+                            {dateRender && <HumidityChart dateFrom={dateFrom} dateTo={dateTo} />}
+                        </div>
                     </div>
-                    <p></p>
                     {/* Dewpoint */}
-                    <div className="form-check">
-                        <input 
-                          className="form-check-input" 
-                          type="checkbox" 
-                          id="dew"
-                          onChange={() => toggleChart('#dewChart')}
-                        />
-                        <label className="form-check-label" htmlFor="dew">
-                            Dewpoint
-                        </label>
+                    <div id="dewChart">
+                        <h2>Dewpoint</h2>
+                        <hr />
+                        <div className="chart-container" style={{ height: '400px' }}>
+
+                        </div>
                     </div>
-                    <p></p>
                     {/* Pressure */}
-                    <div className="form-check">
-                        <input 
-                          className="form-check-input" 
-                          type="checkbox" 
-                          id="pressure"
-                          onChange={() => toggleChart('#pressureChart')}
-                        />
-                        <label className="form-check-label" htmlFor="pressure">
-                            Pressure
-                        </label>
-                    </div>
-                </div>
-            </div>
-            {/* Sidebar Footer */}
-            <div className="col-sm-2 setToBottom">
-                <div className ="dates">
-                    <h4>From</h4>
-                    <input type="date" id="from" className="dateInput" value={dateFrom} onChange={handleDateChange}></input> to <input type="date" id="to" className="dateInput"value={dateTo} onChange={handleDateChange}></input>
-                </div>
-                <hr />
-                <div className="sideLink">
-                    <a href="https://github.com/avryr/bera">Link to our GitHub...</a>
-                </div>
-            </div>
-            {/* Charts */}
-            <div className="col-sm-10 charts">
-                {/* Precipitation */}
-                <div id="precipChart">
-                    <h2>Precipitation</h2>
-                    <hr />
-                </div>
-                {/* Temperature */}
-                <div id="tempChart">
-                    <h2>Temperature</h2>
-                    <hr />
-                    <div className="chart-container" style={{ height: '400px' }}>
-                        {dateRender && <TemperatureChart dateFrom={dateFrom} dateTo={dateTo}/>}
-                    </div>
-                </div>
-                {/* Humidity */}
-                <div id="humidChart">
-                    <h2>Humidity</h2>
-                    <hr />
-                    <div className="chart-container" style={{ height: '400px' }}>
-                        {dateRender && <HumidityChart dateFrom={dateFrom} dateTo={dateTo}/>}
-                    </div>
-                </div>
-                {/* Dewpoint */}
-                <div id="dewChart">
-                    <h2>Dewpoint</h2>
-                    <hr />
-                    <div className="chart-container" style={{ height: '400px' }}>
-                        
-                    </div>
-                </div>
-                {/* Pressure */}
-                <div id="pressureChart">
-                    <h2>Presure</h2>
-                    <hr />
-                    <div className="chart-container" style={{ height: '400px' }}>
-                        
+                    <div id="pressureChart">
+                        <h2>Presure</h2>
+                        <hr />
+                        <div className="chart-container" style={{ height: '400px' }}>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
